1de5b7cb23bec191047936aba9436a7ad9b5840e
allymf <alyssonfacanhamoreira@gmail.com>
2024-06-03 16:12:03 -0300

    feat(cora-button): Create extension to contain AppColors

--- CoraChallenge/CoraChallenge/Commom/UIColor+AppColors.swift
+++ CoraChallenge/CoraChallenge/Commom/UIColor+AppColors.swift
@@ -1,8 +1,17 @@
-//
-//  UIColor+AppColors.swift
-//  CoraChallenge
-//
-//  Created by Alysson Moreira on 03/06/24.
-//
+import UIKit
 
-import Foundation
+extension UIColor {
+    convenience public init(rgb: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((rgb >> 16) & 0xFF) / 255.0,
+            green: CGFloat((rgb >> 8) & 0xFF) / 255.0,
+            blue: CGFloat(rgb & 0xFF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
+
+extension UIColor {
+    static var mainPink = UIColor(rgb: 0xFE3E6D)
+}
+
