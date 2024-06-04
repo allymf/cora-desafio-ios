9d4ec5f361b22f7d9a717c2a4e1ab6fb0e6d4d46
allymf <alyssonfacanhamoreira@gmail.com>
2024-06-03 22:52:55 -0300

    test(welcome-logic): Create unit tests for WelcomeScene Interactor and Presenter

---
+++ CoraChallenge/CoraChallengeTests/Scenes/WelcomePresenterTests.swift
@@ -0,0 +1,41 @@
+//
+//  WelcomePresenterTests.swift
+//  CoraChallengeTests
+//
+//  Created by Alysson Moreira on 03/06/24.
+//
+
+import XCTest
+@testable import CoraChallenge
+
+final class WelcomePresenterTests: XCTestCase {
+    private let displayerSpy = DisplayerSpy()
+    private lazy var sut = {
+        let presenter = WelcomePresenter()
+        presenter.displayer = displayerSpy
+        return presenter
+    }()
+    
+    func test_presentLogin_whenMethodIsCalled_thenItShouldCallCorrectMethodFromDisplayer() {
+        // Given
+        let expectedDisplayLoginCalls = 1
+        
+        // When
+        sut.presentLogin()
+        
+        // Then
+        XCTAssertEqual(displayerSpy.displayLoginCalls, expectedDisplayLoginCalls)
+    }
+    
+}
+
+extension WelcomePresenterTests {
+    private final class DisplayerSpy: WelcomeDisplayLogic {
+        
+        private(set) var displayLoginCalls = 0
+        func displayLogin() {
+            displayLoginCalls += 1
+        }
+        
+    }
+}
