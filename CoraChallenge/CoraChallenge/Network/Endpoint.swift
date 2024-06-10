5707928585dab7656d2dedbf8668a7b5dc7a2052
allymf <alyssonfacanhamoreira@gmail.com>
2024-06-10 18:02:19 -0300

    feat(statement-business-logic): Implement StatementWorker

--- CoraChallenge/CoraChallenge/Network/Endpoint.swift
+++ CoraChallenge/CoraChallenge/Network/Endpoint.swift
@@ -4,5 +4,6 @@ public protocol Endpoint {
     var path: String { get }
     var urlParameters: [String: String]? { get }
     var bodyParameters: [String: String]? { get }
+    var headerParameters: [String: String]? { get }
     var method: HTTPMethod { get }
 }
--- CoraChallenge/CoraChallenge/Scenes/Statement/StatementInteractor.swift
+++ CoraChallenge/CoraChallenge/Scenes/Statement/StatementInteractor.swift
@@ -8,13 +8,16 @@ final class StatementInteractor: StatementBusinessLogic, StatementDataStore {
     
     private let presenter: StatementPresentationLogic
     private let worker: StatementWorkingLogic
+    private let tokenStorage: TokenStoring
     
     init(
         presenter: StatementPresentationLogic,
-        worker: StatementWorkingLogic
+        worker: StatementWorkingLogic,
+        tokenStorage: TokenStoring = TokenStorage()
     ) {
         self.presenter = presenter
         self.worker = worker
+        self.tokenStorage = tokenStorage
     }
     
 }
